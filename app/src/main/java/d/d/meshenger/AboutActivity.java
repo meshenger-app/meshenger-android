a6a8e5f2eaede7a0c584e4514c52265a2ae4f450
Moritz Warning <moritzwarning@web.de>
2019-09-09 02:24:30 +0200

    remove authors
    the list is getting too crowded

--- app/src/main/java/d/d/meshenger/AboutActivity.java
+++ app/src/main/java/d/d/meshenger/AboutActivity.java
@@ -1,7 +1,6 @@
 package d.d.meshenger;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -15,36 +14,13 @@ public class AboutActivity extends MeshengerActivity {
 
         setTitle(getResources().getString(R.string.menu_about));
 
-        findViewById(R.id.mailAddress1).setOnClickListener(v -> sendMail1());
-        findViewById(R.id.mailAddress2).setOnClickListener(v -> sendMail2());
-        findViewById(R.id.mailAddress3).setOnClickListener(v -> sendMail3());
-        findViewById(R.id.licenseVersion).setOnClickListener(v -> showLicense());
-
         ((TextView) findViewById(R.id.versionTv)).setText(
             Utils.getApplicationVersion(this)
         );
-    }
 
-    private void showLicense() {
-        Intent intent = new Intent(this, LicenseActivity.class);
-        startActivity(intent);
-    }
-
-    private void sendMail1() {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:dakhnod@gmail.com"));
-        startActivity(intent);
-    }
-
-    private void sendMail2() {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:vasu.hvardhan@gmail.com"));
-        startActivity(intent);
-    }
-
-    private void sendMail3() {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:moritzwarning@web.de"));
-        startActivity(intent);
+        findViewById(R.id.licenseTV).setOnClickListener(v -> {
+            Intent intent = new Intent(this, LicenseActivity.class);
+            startActivity(intent);
+        });
     }
 }

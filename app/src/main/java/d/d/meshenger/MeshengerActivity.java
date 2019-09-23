f8835151e3178603afcffc1cd63a4601676ee8c2
Moritz Warning <moritzwarning@web.de>
2019-09-23 17:54:58 +0200

    fix dark mode bug
    the setting was not applied to paused activities

--- app/src/main/java/d/d/meshenger/MeshengerActivity.java
+++ app/src/main/java/d/d/meshenger/MeshengerActivity.java
@@ -10,10 +10,29 @@ import android.support.v7.app.AppCompatDelegate;
  * Activity base class to apply night mode
 */
 public class MeshengerActivity extends AppCompatActivity {
+    boolean dark_active; // dark mode
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setTheme(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES ? R.style.AppTheme_Dark : R.style.AppTheme_Light);
+        dark_active = darkModeEnabled();
+        setTheme(dark_active ? R.style.AppTheme_Dark : R.style.AppTheme_Light);
+    }
+
+    private boolean darkModeEnabled() {
+        return (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // recreate acitivity to apply mode
+        boolean dark_active_now = darkModeEnabled();
+        if (dark_active != dark_active_now) {
+            dark_active = dark_active_now;
+            recreate();
+        }
     }
 }

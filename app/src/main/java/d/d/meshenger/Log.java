9c65882eb60877f7b9e0273c33ba3dd981d84587
Moritz Warning <moritzwarning@web.de>
2019-11-10 17:50:03 +0100

    extend logging wrapper

--- app/src/main/java/d/d/meshenger/Log.java
+++ app/src/main/java/d/d/meshenger/Log.java
@@ -18,4 +18,25 @@ public class Log {
             android.util.Log.d(tag, message);
         }
     }
+
+    public static void w(Object context, String message) {
+        if (BuildConfig.DEBUG) {
+            String tag = contextString(context);
+            android.util.Log.w(tag, message);
+        }
+    }
+
+    public static void i(Object context, String message) {
+        if (BuildConfig.DEBUG) {
+            String tag = contextString(context);
+            android.util.Log.i(tag, message);
+        }
+    }
+
+    public static void e(Object context, String message) {
+        if (BuildConfig.DEBUG) {
+            String tag = contextString(context);
+            android.util.Log.e(tag, message);
+        }
+    }
 }

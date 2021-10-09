f39ec1d718c49db0778b73f382ef6b18e66fb016
vadym <vadym.vikulin@gmail.com>
2021-10-09 16:07:44 +0300

    1. enabled speaker mode

--- app/src/main/java/d/d/meshenger/RTCCall.java
+++ app/src/main/java/d/d/meshenger/RTCCall.java
@@ -59,6 +59,7 @@ public class RTCCall implements DataChannel.Observer {
     private MediaStream upStream;
     private DataChannel dataChannel;
 
+    private boolean speakerEnabled;
     private boolean videoEnabled;
 
     private Context context;
@@ -351,8 +352,16 @@ public class RTCCall implements DataChannel.Observer {
         });
     }
 
+    public boolean isSpeakerEnabled(){
+        return this.speakerEnabled;
+    }
+
     public boolean isVideoEnabled(){
         return this.videoEnabled;
+    }
+
+    public void setSpeakerEnabled(boolean enabled) {
+        this.speakerEnabled = enabled;
     }
 
     public void setVideoEnabled(boolean enabled) {

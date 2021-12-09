0ad1e179ebf84593c08f3f0ebc06e86c54e6a612
vadym <vadym.vikulin@gmail.com>
2021-12-09 11:34:57 +0200

    1. icon fix

--- app/src/main/java/org/rivchain/cuplink/MainApplication.java
+++ app/src/main/java/org/rivchain/cuplink/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import org.acra.ACRA;
+import org.acra.BuildConfig;
 import org.acra.config.CoreConfigurationBuilder;
 import org.acra.config.DialogConfigurationBuilder;
 import org.acra.config.HttpSenderConfigurationBuilder;

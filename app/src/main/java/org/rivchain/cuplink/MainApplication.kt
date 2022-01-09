eeabf9014cb916a936712c238fac63d2398d180e
vadym <vadym.vikulin@gmail.com>
2022-01-10 00:21:36 +0200

    1. added ACRA dialog fix

--- app/src/main/java/org/rivchain/cuplink/MainApplication.kt
+++ app/src/main/java/org/rivchain/cuplink/MainApplication.kt
@@ -1,35 +1,44 @@
 package org.rivchain.cuplink
 
-import android.R
 import android.app.Application
 import android.content.Context
-import org.acra.ACRA.init
 import org.acra.BuildConfig
-import org.acra.config.CoreConfigurationBuilder
-import org.acra.config.DialogConfigurationBuilder
-import org.acra.config.HttpSenderConfigurationBuilder
+import org.acra.config.*
 import org.acra.data.StringFormat
+import org.acra.ktx.initAcra
 import org.acra.sender.HttpSender
 
 class MainApplication : Application() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
-        val builder = CoreConfigurationBuilder(this)
-        builder.withBuildConfigClass(BuildConfig::class.java)
-                .withReportFormat(StringFormat.JSON)
-        val plugin = builder.getPluginConfigurationBuilder(HttpSenderConfigurationBuilder::class.java)
-                .withUri("http://acrarium.rivchain.org/acrarium-1.4.6/report")
-                .withEnabled(true)
-        plugin.basicAuthLogin = "KOF7CEnt5tfTqIhj"
-        plugin.basicAuthPassword = "F4cCIqo9EjpihcPt"
-        plugin.httpMethod = HttpSender.Method.POST
-        val dialog = DialogConfigurationBuilder(this)
-        dialog.withText("Sorry, the application crashed.")
-        dialog.withTitle("CupLink")
-        dialog.withPositiveButtonText(base.getText(R.string.ok).toString())
-        dialog.withNegativeButtonText(base.getText(R.string.cancel).toString())
-        dialog.commentPrompt = "Please describe what were you doing when the app crashed:"
-        dialog.withResIcon(R.drawable.ic_dialog_alert)
-        init(this, builder)
+        initAcra {
+            buildConfigClass = BuildConfig::class.java
+            reportFormat = StringFormat.JSON
+            //each plugin you chose above can be configured in a block like this:
+            httpSender {
+                uri = "http://acrarium.rivchain.org/acrarium-1.4.6/report"
+                basicAuthLogin="KOF7CEnt5tfTqIhj"
+                basicAuthPassword = "F4cCIqo9EjpihcPt"
+                httpMethod = HttpSender.Method.POST
+            }
+            dialog {
+                //required
+                text = getString(R.string.report_dialog_text)
+                //optional, enables the dialog title
+                title = getString(R.string.app_name)
+                //defaults to android.R.string.ok
+                positiveButtonText = getString(android.R.string.ok)
+                //defaults to android.R.string.cancel
+                negativeButtonText = getString(android.R.string.cancel)
+                //optional, enables the comment input
+                commentPrompt = getString(R.string.report_dialog_comment)
+                //optional, enables the email input
+                //emailPrompt = getString(R.string.report_dialog_email)
+                //defaults to android.R.drawable.ic_dialog_alert
+                resIcon = android.R.drawable.ic_dialog_alert
+                //optional, defaults to @android:style/Theme.Dialog
+                resTheme = R.style.AppTheme
+            }
+        }
     }
 }

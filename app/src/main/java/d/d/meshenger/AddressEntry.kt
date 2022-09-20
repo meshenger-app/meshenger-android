c6ecf51e94cfe309e954c2f6bea574db96befaed
Moritz Warning <moritzwarning@web.de>
2022-09-20 15:21:42 +0200

    AddressEntry: cleanup

--- app/src/main/java/d/d/meshenger/AddressEntry.kt
+++ app/src/main/java/d/d/meshenger/AddressEntry.kt
@@ -7,7 +7,6 @@ class AddressEntry internal constructor(
     var address: String,
     var device: String,
     var multicast: Boolean,
-    val isCustom: Boolean = false,
 ) : Comparable<AddressEntry> {
     override fun compareTo(e: AddressEntry): Int {
         return address.compareTo(e.address)
@@ -27,30 +26,5 @@ class AddressEntry internal constructor(
 
     override fun hashCode(): Int {
         return address.hashCode()
-    }
-
-    companion object {
-        fun findAddressEntry(list: List<AddressEntry>, address: String): AddressEntry? {
-            var i = 0
-            while (i < list.size) {
-                if (list[i].address == address) {
-                    return list[i]
-                }
-                i += 1
-            }
-            return null
-        }
-
-        fun listIndexOf(list: List<AddressEntry>, entry: AddressEntry): Int {
-            val iter: Iterator<AddressEntry> = list.listIterator()
-            var i = 0
-            while (iter.hasNext()) {
-                if (iter.next().address == entry.address) {
-                    return i
-                }
-                i += 1
-            }
-            return -1
-        }
     }
 }

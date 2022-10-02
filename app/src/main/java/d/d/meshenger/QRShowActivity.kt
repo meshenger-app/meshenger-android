236a0186df5e86059e8352f656165d55f69d2b6c
Moritz Warning <moritzwarning@web.de>
2022-10-02 22:27:33 +0200

    QRShowActivity.kt: cleanup

--- app/src/main/java/d/d/meshenger/QRShowActivity.kt
+++ app/src/main/java/d/d/meshenger/QRShowActivity.kt
@@ -4,19 +4,16 @@ import android.content.*
 import android.os.Bundle
 import android.os.IBinder
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import d.d.meshenger.MainService.MainBinder
 
 class QRShowActivity : MeshengerActivity(), ServiceConnection {
-    private var contact: Contact? = null
+    private var extra_contact: Contact? = null
     private var binder: MainBinder? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,28 +21,22 @@ class QRShowActivity : MeshengerActivity(), ServiceConnection {
         setContentView(R.layout.activity_qrshow)
 
         if (intent.hasExtra("EXTRA_CONTACT")) {
-            contact = intent.extras!!["EXTRA_CONTACT"] as Contact?
-            findViewById<View>(R.id.fabPresenter).visibility = View.GONE
-            val params = RelativeLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
-            params.addRule(RelativeLayout.ALIGN_PARENT_END, RelativeLayout.TRUE)
-            params.rightMargin = 80
-            params.bottomMargin = params.rightMargin
-            findViewById<View>(R.id.fabShare).layoutParams = params
+            extra_contact = intent.extras!!["EXTRA_CONTACT"] as Contact?
         }
+
         setTitle(getString(R.string.scan_invitation))
 
         bindService()
+
         findViewById<View>(R.id.fabPresenter).setOnClickListener { _: View? ->
             startActivity(Intent(this, QRScanActivity::class.java))
             finish()
         }
+
         findViewById<View>(R.id.fabShare).setOnClickListener { _: View? ->
-            if (contact != null) try {
-                val data = Contact.toJSON(contact!!, false).toString()
+            try {
+                val contact = extra_contact ?: binder!!.getSettings().getOwnContact()
+                val data = Contact.toJSON(contact, false).toString()
                 val i = Intent(Intent.ACTION_SEND)
                 i.putExtra(Intent.EXTRA_TEXT, data)
                 i.type = "text/plain"
@@ -69,18 +60,9 @@ class QRShowActivity : MeshengerActivity(), ServiceConnection {
         bindService(serviceIntent, this, BIND_AUTO_CREATE)
     }
 
-    private fun generateQR() {
+    private fun generateQR(contact: Contact) {
         val name_tv = findViewById<TextView>(R.id.contact_name_tv)
-        val contact_own = binder!!.getSettings().getOwnContact()
-        val contact_other = this.contact
-
-        val contact = if (contact_other != null) {
-            name_tv.text = contact_other.name
-            contact_other
-        } else {
-            name_tv.text = contact_own.name
-            contact_own
-        }
+        name_tv.text = contact.name
 
         val data = Contact.toJSON(contact, false).toString()
         val multiFormatWriter = MultiFormatWriter()
@@ -96,8 +78,10 @@ class QRShowActivity : MeshengerActivity(), ServiceConnection {
 
     override fun onServiceConnected(componentName: ComponentName, iBinder: IBinder) {
         binder = iBinder as MainBinder
+
         try {
-            generateQR()
+            val contact = extra_contact ?: binder!!.getSettings().getOwnContact()
+            generateQR(contact)
         } catch (e: Exception) {
             e.printStackTrace()
             Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()

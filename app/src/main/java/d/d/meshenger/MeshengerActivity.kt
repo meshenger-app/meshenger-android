8204039856cd54130968fcf5d9b9bb4e1ca266a3
Moritz Warning <moritzwarning@web.de>
2022-10-10 21:14:37 +0200

    MeshengerActivity.kt: minor cleanup

--- app/src/main/java/d/d/meshenger/MeshengerActivity.kt
+++ app/src/main/java/d/d/meshenger/MeshengerActivity.kt
@@ -2,20 +2,22 @@ package d.d.meshenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import d.d.meshenger.R
 import androidx.appcompat.app.AppCompatDelegate
 
 /*
  * Activity base class to apply night mode
 */
 open class MeshengerActivity : AppCompatActivity() {
-    var dark_active // dark mode
-            = false
+    var dark_active = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dark_active = darkModeEnabled()
-        setTheme(if (dark_active) R.style.AppTheme_Dark_NoActionBar else R.style.AppTheme_Light_NoActionBar)
+        if (dark_active) {
+            setTheme(R.style.AppTheme_Dark_NoActionBar)
+        } else {
+            setTheme(R.style.AppTheme_Light_NoActionBar)
+        }
     }
 
     private fun darkModeEnabled(): Boolean {
@@ -25,7 +27,7 @@ open class MeshengerActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        // recreate acitivity to apply mode
+        // recreate activity to apply mode
         val dark_active_now = darkModeEnabled()
         if (dark_active != dark_active_now) {
             dark_active = dark_active_now

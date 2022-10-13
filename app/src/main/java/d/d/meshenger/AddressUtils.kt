db6647bc3c47ac1f4ec129fb61ff38c4cb6d91f9
Moritz Warning <moritzwarning@web.de>
2022-10-13 18:02:05 +0200

    AddressUtils.kt: fix mac address format

--- app/src/main/java/d/d/meshenger/AddressUtils.kt
+++ app/src/main/java/d/d/meshenger/AddressUtils.kt
@@ -55,7 +55,7 @@ internal object AddressUtils
 
     private fun formatMAC(mac: ByteArray): String {
         return String.format("%02X:%02X:%02X:%02X:%02X:%02X",
-            (mac[0] xor 2), mac[1], mac[2], mac[3], mac[4], mac[5])
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])
     }
 
     private fun macAddressToBytes(macAddress: String): ByteArray? {

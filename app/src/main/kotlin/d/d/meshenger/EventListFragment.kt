02082e88598d6641f61e0f728c465a787e8eb9a1
Moritz Warning <moritzwarning@web.de>
2023-02-12 16:40:17 +0100

    EventListFragment.kt: move out listeners into lambda

--- app/src/main/kotlin/d/d/meshenger/EventListFragment.kt
+++ app/src/main/kotlin/d/d/meshenger/EventListFragment.kt
@@ -15,10 +15,80 @@ import androidx.fragment.app.Fragment
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class EventListFragment : Fragment(), AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+class EventListFragment : Fragment() {
     private lateinit var eventListAdapter: EventListAdapter
     private lateinit var eventListView: ListView
     private lateinit var fabDelete: FloatingActionButton
+
+    private val onEventClickListener = AdapterView.OnItemClickListener { _, _, i, _ ->
+        Log.d(this, "onItemClick")
+        val activity = requireActivity()
+        val binder = (activity as MainActivity).binder ?: return@OnItemClickListener
+        val eventGroup = eventListAdapter.getItem(i)
+        val latestEvent = eventGroup.last()
+
+        val knownContact = binder.getContacts().getContactByPublicKey(latestEvent.publicKey)
+        val contact = if (knownContact != null) {
+            knownContact
+        } else {
+            latestEvent.createUnknownContact("")
+        }
+
+        if (contact.addresses.isEmpty()) {
+            Toast.makeText(activity, R.string.contact_has_no_address_warning, Toast.LENGTH_SHORT).show()
+        } else {
+            // start call
+            Log.d(this, "start CallActivity")
+            val intent = Intent(activity, CallActivity::class.java)
+            intent.action = "ACTION_OUTGOING_CALL"
+            intent.putExtra("EXTRA_CONTACT", contact)
+            startActivity(intent)
+        }
+    }
+
+    private val onContactLongClickListener = AdapterView.OnItemLongClickListener { adapterView, view, i, _ ->
+        Log.d(this, "onItemLongClick")
+        val activity = requireActivity()
+        val binder = (activity as MainActivity).binder ?: return@OnItemLongClickListener false
+
+        val event = adapterView.adapter.getItem(i) as Event
+        val menu = PopupMenu(activity, view)
+        val res = resources
+        val add = res.getString(R.string.add)
+        val block = res.getString(R.string.block)
+        val unblock = res.getString(R.string.unblock)
+        val contact = binder.getContacts().getContactByPublicKey(event.publicKey)
+
+        // allow to add unknown caller
+        if (contact == null) {
+            menu.menu.add(add)
+        }
+
+        if (contact != null) {
+            if (contact.blocked) {
+                menu.menu.add(unblock)
+            } else {
+                menu.menu.add(block)
+            }
+        }
+
+        menu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.title.toString()) {
+                add -> {
+                    showAddDialog(event)
+                }
+                block -> {
+                    setBlocked(event, true)
+                }
+                unblock -> {
+                    setBlocked(event, false)
+                }
+            }
+            false
+        }
+        menu.show()
+        true
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,8 +113,8 @@ class EventListFragment : Fragment(), AdapterView.OnItemClickListener, AdapterVi
 
         eventListAdapter = EventListAdapter(activity, R.layout.item_event, emptyList(), emptyList())
         eventListView.adapter = eventListAdapter
-        eventListView.onItemClickListener = this
-        eventListView.onItemLongClickListener = this
+        eventListView.onItemClickListener = onEventClickListener
+        eventListView.onItemLongClickListener = onContactLongClickListener
 
         LocalBroadcastManager.getInstance(requireContext())
             .registerReceiver(refreshEventListReceiver, IntentFilter("refresh_event_list"))
@@ -143,76 +213,5 @@ class EventListFragment : Fragment(), AdapterView.OnItemClickListener, AdapterVi
         }
         exitButton.setOnClickListener { dialog.dismiss() }
         dialog.show()
-    }
-
-    override fun onItemLongClick(adapterView: AdapterView<*>, view: View, i: Int, l: Long): Boolean {
-        Log.d(this, "onItemLongClick")
-        val activity = requireActivity()
-        val binder = (activity as MainActivity).binder ?: return false
-
-        val event = adapterView.adapter.getItem(i) as Event
-        val menu = PopupMenu(activity, view)
-        val res = resources
-        val add = res.getString(R.string.add)
-        val block = res.getString(R.string.block)
-        val unblock = res.getString(R.string.unblock)
-        val contact = binder.getContacts().getContactByPublicKey(event.publicKey)
-
-        // allow to add unknown caller
-        if (contact == null) {
-            menu.menu.add(add)
-        }
-
-        if (contact != null) {
-            if (contact.blocked) {
-                menu.menu.add(unblock)
-            } else {
-                menu.menu.add(block)
-            }
-        }
-
-        menu.setOnMenuItemClickListener { menuItem: MenuItem ->
-            when (menuItem.title.toString()) {
-                add -> {
-                    showAddDialog(event)
-                }
-                block -> {
-                    setBlocked(event, true)
-                }
-                unblock -> {
-                    setBlocked(event, false)
-                }
-            }
-            false
-        }
-        menu.show()
-        return true
-    }
-
-    override fun onItemClick(adapterView: AdapterView<*>?, view: View, i: Int, l: Long) {
-        Log.d(this, "onItemClick")
-        val activity = requireActivity()
-        val binder = (activity as MainActivity).binder ?: return
-        val eventGroup = eventListAdapter.getItem(i)
-        val latestEvent = eventGroup.last()
-
-        val knownContact = binder.getContacts().getContactByPublicKey(latestEvent.publicKey)
-        val contact = if (knownContact != null) {
-            knownContact
-        } else {
-            latestEvent.createUnknownContact("")
-        }
-
-        if (contact.addresses.isEmpty()) {
-            Toast.makeText(activity, R.string.contact_has_no_address_warning, Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        // start call
-        Log.d(this, "start CallActivity")
-        val intent = Intent(activity, CallActivity::class.java)
-        intent.action = "ACTION_OUTGOING_CALL"
-        intent.putExtra("EXTRA_CONTACT", contact)
-        startActivity(intent)
     }
 }

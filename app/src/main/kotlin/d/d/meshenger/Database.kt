62c73078ce52ef6a0c8bb9c9a42d3b8676751f44
Moritz Warning <moritzwarning@web.de>
2024-08-05 23:37:38 +0200

    bump to 4.3.4

--- app/src/main/kotlin/d/d/meshenger/Database.kt
+++ app/src/main/kotlin/d/d/meshenger/Database.kt
@@ -309,6 +309,11 @@ class Database {
                 newFrom = "4.3.3"
             }
 
+            if (newFrom == "4.3.3") {
+                // nothing to do
+                newFrom = "4.3.4"
+            }
+
             alignSettings(settings)
 
             db.put("version", newFrom)

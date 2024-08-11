6ceecff33e61fb1c391670449754e0b28df879e9
Moritz Warning <moritzwarning@web.de>
2024-08-12 00:49:01 +0200

    DefaultObserver: add more debug output

--- app/src/main/kotlin/d/d/meshenger/call/DefaultObserver.kt
+++ app/src/main/kotlin/d/d/meshenger/call/DefaultObserver.kt
@@ -1,5 +1,6 @@
 package d.d.meshenger.call
 
+import d.d.meshenger.Log
 import org.webrtc.DataChannel
 import org.webrtc.IceCandidate
 import org.webrtc.MediaStream
@@ -7,12 +8,24 @@ import org.webrtc.PeerConnection.*
 import org.webrtc.RtpReceiver
 
 internal open class DefaultObserver : Observer {
-    override fun onSignalingChange(signalingState: SignalingState) {}
-    override fun onIceConnectionChange(iceConnectionState: IceConnectionState) {}
-    override fun onIceConnectionReceivingChange(b: Boolean) {}
-    override fun onIceGatheringChange(iceGatheringState: IceGatheringState) {}
-    override fun onIceCandidate(iceCandidate: IceCandidate) {}
-    override fun onIceCandidatesRemoved(iceCandidates: Array<IceCandidate>) {}
+    override fun onSignalingChange(signalingState: SignalingState) {
+        Log.d(this, "onSignalingChange: $signalingState")
+    }
+    override fun onIceConnectionChange(iceConnectionState: IceConnectionState) {
+        Log.d(this, "onIceConnectionChange: $iceConnectionState")
+    }
+    override fun onIceConnectionReceivingChange(b: Boolean) {
+        Log.d(this, "onIceConnectionReceivingChange: $b")
+    }
+    override fun onIceGatheringChange(iceGatheringState: IceGatheringState) {
+        Log.d(this, "onIceGatheringChange: $iceGatheringState")
+    }
+    override fun onIceCandidate(iceCandidate: IceCandidate) {
+        Log.d(this, "onIceCandidate: $iceCandidate")
+    }
+    override fun onIceCandidatesRemoved(iceCandidates: Array<IceCandidate>) {
+        Log.d(this, "onIceCandidatesRemoved: ${iceCandidates.joinToString()}")
+    }
     override fun onAddStream(mediaStream: MediaStream) {}
     override fun onRemoveStream(mediaStream: MediaStream) {}
     override fun onDataChannel(dataChannel: DataChannel) {}
